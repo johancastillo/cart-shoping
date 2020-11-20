34602925b1319b0b2c10d3f20b1ffc4b049f84b6
Johan Castillo <jcjohan2707@gmail.com>
2020-11-20 14:30:56 -0400

    ERRORS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Container, Grid, Header } from 'semantic-ui-react'
-import Menu from '../Menu'
-import ProductList from '../ProductList'
-import CartList from '../CartList'
-import Order from '../Order'
+import Menu from './components/Menu'
+import ProductList from './components/ProductList'
+import CartList from './components/CartList'
+import Order from './components/Order'
 import style from './App.css'
+import {products} from './data.json'
 
 class App extends Component {
   constructor(props) {
@@ -13,108 +14,7 @@ class App extends Component {
       openOrder: false,
       total: 0,
       sum: 0,
-      products: [
-        {
-          id: 1,
-          name: 'Nikon D5500 24.2 Mp',
-          picture: 'https://http2.mlstatic.com/camara-D_Q_NP_100101-MLM20255371968_032015-X.webp',
-          price: 12346,
-          datails: 'Compra Protegida, recibe el producto que esperabas o te devolvemos tu dinero.',
-          marca: 'Nikon',
-          status: 5,
-        },
-        {
-          id: 2,
-          name: 'Sony Dsc-h300 20 Mp',
-          picture: 'https://http2.mlstatic.com/camara-D_Q_NP_621585-MLM25970442208_092017-X.webp',
-          price: 3938,
-          datails: 'Compra Protegida, recibe el producto que esperabas o te devolvemos tu dinero.',
-          marca: 'Sony',
-          status: 6,
-        },
-        {
-          id: 3,
-          name: 'Fujifilm X-t10 40.0 Mp',
-          picture: 'https://http2.mlstatic.com/camara-D_Q_NP_997939-MLM25733136676_072017-X.webp',
-          price: 18999,
-          datails: 'Compra Protegida, recibe el producto que esperabas o te devolvemos tu dinero.',
-          marca: 'Fujifilm',
-          status: 2,
-        },
-        {
-          id: 4,
-          name: 'Reflex Canon Eos 30 ',
-          picture: 'https://http2.mlstatic.com/camara-D_Q_NP_743505-MLM25028787002_082016-X.webp',
-          price: 1000,
-          datails: 'Compra Protegida, recibe el producto que esperabas o te devolvemos tu dinero.',
-          marca: 'Canon',
-          status: 6,
-        },
-        {
-          id: 5,
-          name: 'Holster Camera Case ',
-          picture: 'https://images-na.ssl-images-amazon.com/images/I/91cMeS6xLHL._SX522_.jpg',
-          price: 328,
-          marca: 'Amazon',
-          status: 10,
-        },
-        {
-          id: 6,
-          name: 'Olympus M. 40-150mm ',
-          picture: 'https://images-na.ssl-images-amazon.com/images/I/81vy-AiCWSL._AC_UL160_SR160,160_.jpg',
-          price: 2733,
-          marca: 'Olympus',
-          status: 4,
-        },
-        {
-          id: 7,
-          name: 'Tripié ligero ',
-          picture: 'https://images-na.ssl-images-amazon.com/images/G/33/aplusautomation/vendorimages/2ad656e6-6b25-48fd-af65-0a113979bc54.jpg._CB293876631__SR300,300_.jpg',
-          price: 372,
-          marca: 'Amazon',
-          status: 2,
-        },
-        {
-          id: 8,
-          name: 'Fujinon XF10-24MMF4 ',
-          picture: 'https://images-na.ssl-images-amazon.com/images/I/81tbbvcsJIL._AC_UL160_SR160,160_.jpg',
-          price: 25023,
-          marca: 'Fujinon',
-          status: 4,
-        },
-        {
-          id: 9,
-          name: 'Fujifilm X-T2 Mirrorless ',
-          picture: 'https://images-na.ssl-images-amazon.com/images/I/81CUJvIrFML._AC_UL160_SR160,160_.jpg',
-          price: 50399,
-          marca: 'Fujifilm',
-          status: 3,
-        },
-        {
-          id: 10,
-          name: 'LUMIX G Lens, 25mm',
-          picture: 'https://images-na.ssl-images-amazon.com/images/I/71dTWmH4vxL._SL1500_.jpg',
-          price: 3262,
-          marca: 'Panasonic',
-          status: 4,
-        },
-        {
-          id: 11,
-          name: 'Olympus OM-D E-M5 ',
-          picture: 'https://images-na.ssl-images-amazon.com/images/I/918N6QYcTjL._SL1500_.jpg',
-          price: 18688,
-          marca: 'Olympus',
-          status: 3,
-        },
-        {
-          id: 12,
-          name: 'Fujifilm X-T20 ',
-          picture: 'https://images-na.ssl-images-amazon.com/images/I/91in%2BYvDGtL._AC_UL160_SR160,160_.jpg',
-          price: 32699,
-          marca: 'Fujifilm',
-          status: 10,
-        }
-      ],
+      products,
       cart: [],
     }
 
---
+++ src/components/product-card/ProductCard.jsx
@@ -0,0 +1,120 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import './ProductCard.css';
+
+
+class ProductCard extends Component{
+
+    render(){
+        // variable for the stars
+        let star;
+
+        // Stars validation
+        if(this.props.stars === 1){
+          star = {
+            one: "icon-star-full",
+            two: "icon-star-empty",
+            three: "icon-star-empty",
+            four: "icon-star-empty",
+            five: "icon-star-empty"
+          }
+        }else if(this.props.stars === 1.5){
+          star = {
+            one: "icon-star-full",
+            two: "icon-star-half",
+            three: "icon-star-empty",
+            four: "icon-star-empty",
+            five: "icon-star-empty"
+          }
+        }else if(this.props.stars === 2){
+          star = {
+            one: "icon-star-full",
+            two: "icon-star-full",
+            three: "icon-star-empty",
+            four: "icon-star-empty",
+            five: "icon-star-empty"
+          }
+        }else if(this.props.stars === 2.5){
+          star = {
+            one: "icon-star-full",
+            two: "icon-star-full",
+            three: "icon-star-half",
+            four: "icon-star-empty",
+            five: "icon-star-empty"
+          }
+        }else if(this.props.stars === 3){
+          star = {
+            one: "icon-star-full",
+            two: "icon-star-full",
+            three: "icon-star-full",
+            four: "icon-star-empty",
+            five: "icon-star-empty"
+          }
+        }else if(this.props.stars === 3.5){
+          star = {
+            one: "icon-star-full",
+            two: "icon-star-full",
+            three: "icon-star-full",
+            four: "icon-star-half",
+            five: "icon-star-empty"
+          }
+        }else if(this.props.stars === 4){
+          star = {
+            one: "icon-star-full",
+            two: "icon-star-full",
+            three: "icon-star-full",
+            four: "icon-star-full",
+            five: "icon-star-empty"
+          }
+        }else if(this.props.stars === 4.5){
+          star = {
+            one: "icon-star-full",
+            two: "icon-star-full",
+            three: "icon-star-full",
+            four: "icon-star-full",
+            five: "icon-star-half"
+          }
+        }else if(this.props.stars === 5){
+          star = {
+            one: "icon-star-full",
+            two: "icon-star-full",
+            three: "icon-star-full",
+            four: "icon-star-full",
+            five: "icon-star-full"
+          }
+        }
+
+        // Return View
+        return(
+          <Link to="/product">
+            <div className="card mb-4">
+
+                  <div className="color-gris">  
+                    <img src={this.props.image} className="card-img-top p-2" alt="..." />
+                  </div>
+
+                    <div className="card-body">
+
+                        <span className="card-text name-product">
+                          {this.props.title}
+                        </span>
+
+                        {/** Stars **/}
+                        <div className="star-color">
+                          <span className={star.one}></span>
+                          <span className={star.two}></span>
+                          <span className={star.three}></span>
+                          <span className={star.four}></span>
+                          <span className={star.five}></span>
+                        </div>
+
+                        
+
+                    </div>
+                </div>
+                </Link>
+        )
+    }
+}
+
+export default ProductCard;
